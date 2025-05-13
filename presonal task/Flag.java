5a3d88ae3554fc5f2a673c5698ac6eab0cfaff5a
Macwinner1 <ifechukwuw@gmail.com>
2025-05-13 12:23:40 +0100

    Flag task with loop.

---
+++ presonal task/Flag.java
@@ -0,0 +1,15 @@
+public class Flag{
+public static void main(String[] args){
+
+for(int count = 1; count <= 8; count++){
+System.out.print("* * * * * *");
+System.out.println("===================");
+}
+for(int count1 = 1; count1 <= 5; count1++){
+System.out.println("==============================");
+}
+
+}
+
+
+}
