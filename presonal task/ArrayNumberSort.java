3c3371e3413f083ed3a2d466c92ffdca05bc6bdc
Macwinner1 <ifechukwuw@gmail.com>
2025-05-21 10:09:07 +0100

    complete number sorting in array

---
+++ presonal task/ArrayNumberSort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class ArrayNumberSort{
+public static void main(String[] args){
+double[] list = {9.0, -7.9, 1.0, 5.0, 2.5};
+
+
+SelectionSort.selectionSort(list);
+}
+
+}
