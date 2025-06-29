5ca58bbb9aa6a50b892479071248fdd0070b9e75
Macwinner1 <ifechukwuw@gmail.com>
2025-06-29 16:23:24 +0100

    refactored my method and Test case

--- presonal task/JavaOOP/src/AirConditional/Aircondition.java
+++ presonal task/JavaOOP/src/AirConditional/Aircondition.java
@@ -1,17 +1,46 @@
 package AirConditional;
 
 public class Aircondition {
-    public boolean isSwitchedOff;
+    private boolean isSwitchedOff;
     private int temperatureControl;
+    public int maxTemperature;
+    public int minTemperature;
 
-    public void Aircondition() {
-        int defaultTemperture = 24;
-        temperatureControl = defaultTemperture;
+    public Aircondition() {
+        int DEFAULT_TEMPERATURE = 24;
+        temperatureControl = DEFAULT_TEMPERATURE;
+        maxTemperature = 30;
+        minTemperature = 16;
 
     }
     public boolean switchButtonOn() {
         isSwitchedOff = !isSwitchedOff;
         return isSwitchedOff;
     }
+    public boolean switchButtonOff() {
+        return isSwitchedOff;
+    }
+    public int viewTemperature() {
+        return temperatureControl;
+    }
+    public void incrementTemperature(int increment) {
+        temperatureControl = temperatureControl + increment;
+        if(temperatureControl >= maxTemperature){
+            temperatureControl = maxTemperature;
+        }
+        else {
+            ++temperatureControl;
+        }
+    }
+    public void decrementTemperature(int decrement) {
+        temperatureControl = temperatureControl - decrement;
+        if(temperatureControl <= minTemperature){
+            temperatureControl = minTemperature;
+        }
+        else {
+            --temperatureControl;
+        }
+    }
+
 }
 
--- presonal task/JavaOOP/test/tests/TestAircondition.java
+++ presonal task/JavaOOP/test/tests/TestAircondition.java
@@ -1,4 +1,48 @@
 package tests;
 
+import AirConditional.Aircondition;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestAircondition {
+
+    @Test
+    public void testAirConditional_is_on(){
+        Aircondition aircondition = new Aircondition();
+        assertTrue (aircondition.switchButtonOn());
+    }
+
+    @Test
+    public void testAirconditioner_is_off(){
+        Aircondition aircondition = new Aircondition();
+        assertFalse(aircondition.switchButtonOff());
+    }
+
+    @Test
+    public void testTemperature_is_increase(){
+        Aircondition aircondition = new Aircondition();
+        aircondition.incrementTemperature(2);
+        assertEquals(aircondition.viewTemperature(), 27);
+    }
+
+    @Test
+    public void testTemperature_is_decrease(){
+        Aircondition aircondition = new Aircondition();
+        aircondition.decrementTemperature(2);
+        assertEquals(aircondition.viewTemperature(), 21);
+    }
+
+    @Test
+    public void testTemperature_Max(){
+        Aircondition aircondition = new Aircondition();
+        aircondition.incrementTemperature(8);
+        assertEquals(aircondition.viewTemperature(), 30);
+    }
+    @Test
+    public void testTemperature_Min(){
+        Aircondition aircondition = new Aircondition();
+        aircondition.decrementTemperature(7);
+        assertEquals(aircondition.viewTemperature(), 16);
+    }
+
 }
